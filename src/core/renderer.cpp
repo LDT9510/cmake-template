d45f40007d2bdd8beda641cc2c9511dde7d2fd7e
XReaper95 <luis.ernesto951008@gmail.com>
2025-09-08 23:04:16 -0300

    Hello Triangle: Rectangle using element buffer object (indices)

--- src/core/renderer.cpp
+++ src/core/renderer.cpp
@@ -16,27 +16,30 @@ Renderer::Renderer()
 {
 	glGenVertexArrays(1, &m_vao);
 	glGenBuffers(1, &m_vbo);
+	glGenBuffers(1, &m_ebo);
 }
 
 Renderer::~Renderer()
 {
 	glDeleteVertexArrays(1, &m_vao);
 	glDeleteBuffers(1, &m_vbo);
+	glDeleteBuffers(1, &m_ebo);
 }
 
 void Renderer::setup_rendering() const
 {
 	// clang-format off
 	static constexpr std::array VERTICES = {
-		// first triangle
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-	   -0.5f,  0.5f, 0.0f,  // top left 
-	   // second triangle
-		0.5f, -0.5f, 0.0f,  // bottom right
-	   -0.5f, -0.5f, 0.0f,  // bottom left
-	   -0.5f,  0.5f, 0.0f   // top left
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
    };
+
+	static constexpr std::array INDICES = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
 	// clang-format on
 	// 0. bind Vertex Array Object and copy our vertices array in a buffer for OpenGL to use
 	glBindVertexArray(m_vao);
@@ -45,9 +48,12 @@ void Renderer::setup_rendering() const
 	// 1. then set the vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
 	glEnableVertexAttribArray(0);
-	// 2. use our shader program when we want to render an object
+	// 2. copy our index array in a element buffer for OpenGL to use
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(INDICES), INDICES.data(), GL_STATIC_DRAW);
+	// 3. use our shader program when we want to render an object
 	m_shader.use();
-	// 3. now draw the object  in `Renderer::render()`
+	// 4. now draw the object  in `Renderer::render()`
 
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -60,7 +66,9 @@ void Renderer::render() const
 
 	m_shader.use();
 	glBindVertexArray(m_vao);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
 
 void Renderer::prepare_dev_ui()

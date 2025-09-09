5b18f2c24a16598364c9cd1014eb65cab4b8d525
XReaper95 <luis.ernesto951008@gmail.com>
2025-09-08 23:32:45 -0300

    Shaders: colorful triangle

--- src/core/renderer.cpp
+++ src/core/renderer.cpp
@@ -31,10 +31,10 @@ void Renderer::setup_rendering() const
 {
 	// clang-format off
 	static constexpr std::array VERTICES = {
-		// first triangle
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		// positions         // colors
+		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+	   -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
    };
 
 	// static constexpr std::array INDICES = {  // note that we start from 0!
@@ -46,8 +46,12 @@ void Renderer::setup_rendering() const
 	glBindVertexArray(m_vao);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(VERTICES), VERTICES.data(), GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 }
